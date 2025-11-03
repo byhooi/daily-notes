72c3f535b6a9946bf7cc252872bac3bb55da1bfa
byhooi <byhooi@gmail.com>
2025-11-03 14:08:01 +0800

    fix

--- data/41data.js
+++ data/41data.js
@@ -111,7 +111,7 @@ window.data41 = [
   },
   {
     date: "2025-11-03",
-    title: "（《斑羚飞渡》Ｐ５）",
+    title: "（《斑羚飞渡》P5）",
     content: "黑狼原地<span class='highlight-red'>跳窜</span>起来，以泰山压顶之势<span class='highlight-red'>扑到</span>老羊身上，用两只前爪<span class='highlight-red'>搂抱</span>住羊脖，在老羊还没来得及醒悟过来是怎么回事的时候，就一口<span class='highlight-red'>咬断</span>羊颈上那根脆嫩的动脉血管。"
   }
 ];
